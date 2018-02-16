c8e3695aaf7c64e26b9bca1558c7fc7c2d675f3d
Baptiste Robichon <brobicho@student.le-101.fr>
2018-02-16 16:43:16 +0100

    jmonnerie's special one

--- client.c
+++ client.c
@@ -1,58 +1,50 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   client.c                                         .::    .:/ .      .::   */
+/*   clitest.c                                        .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: brobicho <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/02/15 16:54:08 by brobicho     #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/15 21:00:30 by brobicho    ###    #+. /#+    ###.fr     */
+/*   Created: 2018/02/16 14:59:20 by brobicho     #+#   ##    ##    #+#       */
+/*   Updated: 2018/02/16 16:38:55 by brobicho    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "server.h"
 
-static int	ft_securelen(char *str)
+void	ft_init_struct(struct sockaddr_in *serv_addr)
 {
-	int i;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i])
-		i++;
-	return (i);
+	serv_addr->sin_family = AF_INET;
+	serv_addr->sin_port = htons(25565);
+	serv_addr->sin_addr.s_addr = INADDR_ANY;
 }
 
-int		main(int ac, char **av)
+int		main(void)
 {
-	int					client_fd;
-	int					port;
+	int					server_fd;
+	int					ret;
 	struct sockaddr_in	serv_addr;
 	char				buff[256];
 
-	port = ft_atoi(av[1]);
-	client_fd = socket(AF_INET, SOCK_STREAM, 0);
-	ft_bzero(&serv_addr, sizeof(serv_addr));
-
-	serv_addr.sin_port = htons(port);
-	serv_addr.sin_family = AF_INET;
-
-	inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-
-	connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	ft_init_struct(&serv_addr);
+	ret = connect(server_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	if (ret == -1)
+	{
+		ft_putendl("Connection failed");
+		return (0);
+	}
 	while (1)
 	{
+		bzero(buff, 256);
 		read(0, buff, 256);
-		write(client_fd, &buff, 256);
-		if (!(ft_strncmp(buff, "quit", 256)))
+		if (!strncmp("close", buff, 5))
 			break ;
+		send(server_fd, &buff, 256, 0);
 		bzero(buff, 256);
-		recv(client_fd, buff, 256, 0);
+		recv(server_fd, &buff, 256, 0);
 		ft_putstr(buff);
-		bzero(buff, 256);
 	}
-	close(client_fd);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,50 +6,59 @@
 /*   By: brobicho <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/02/14 13:35:47 by brobicho     #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/15 20:47:50 by brobicho    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/16 16:31:40 by brobicho    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "server.h"
 
-int		main(int ac, char **av)
+static void		ft_server_start(char *buff, int client_fd)
+{
+	while (1)
+	{
+		ft_bzero(buff, 256);
+		read(client_fd, buff, 256);
+		if (!ft_strncmp(buff, "ping", 4) && !ft_isalnum(buff[4]))
+			write(client_fd, "pong pong\n", 10);
+		else if (!ft_strncmp(buff, "quit", 4) && !ft_isalnum(buff[4]))
+		{
+			ft_putendl_fd("Server closed", client_fd);
+			break ;
+		}
+		else
+			write(client_fd, "\0", 1);
+	}
+	close(client_fd);
+}
+
+static void		ft_init_struct(struct sockaddr_in *serv_addr, int port)
+{
+	serv_addr->sin_family = AF_INET;
+	serv_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr->sin_port = htons(port);
+}
+
+int				main(int ac, char **av)
 {
 	int					listen_fd;
 	int					client_fd;
 	struct sockaddr_in	serv_addr;
 	char				buff[256];
-	int					port;
 
 	if (ac != 2)
 	{
 		write(1, "Usage : ./server [port]\n", 25);
 		return (0);
 	}
-	port = ft_atoi(av[1]);
 	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 	ft_bzero(&serv_addr, sizeof(serv_addr));
 	ft_bzero(buff, sizeof(buff));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htons(port);
-
+	ft_init_struct(&serv_addr, ft_atoi(av[1]));
 	bind(listen_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-
 	listen(listen_fd, 10);
 	client_fd = accept(listen_fd, (struct sockaddr*)NULL, NULL);
-	while (1)
-	{
-		ft_bzero(buff, 256);
-		read(client_fd, buff, 256);
-		if (!ft_strncmp(buff, "ping", 4))
-			write(client_fd, "pong pong\n", 10);
-		if (!ft_strncmp(buff, "quit", 4))
-		{
-			write(client_fd, "Server closed\n", 14);
-			break ;
-		}
-	}
-	close(client_fd);
+	ft_server_start(buff, client_fd);
+	close(listen_fd);
 	return (0);
 }
